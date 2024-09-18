2770ca1d68f1e2ddefeeac1d8fa5d9bcbd1048f1
manu <manulol400@gmail.com>
2024-09-18 16:29:41 -0300

    punto 1 completado
    punto 1 listo

--- prueba-tecnica/src/App.jsx
+++ prueba-tecnica/src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import './App.css'
 
 export function App () {
-  const { data } = useFetch('https://catfact.ninja/fact')
+  const { data, loading, error } = useFetch('https://catfact.ninja/fact')
+
+  function primeraPalabra (text) {
+    if (!text) return ''
+
+    let formaPalabra = ''
+
+    for (let i = 0; i < text.length; i++) {
+      if (/^[a-zA-Z]$/.test(text[i])) {
+        formaPalabra += text[i]
+      } else {
+        return formaPalabra
+      }
+    }
+  }
 
   return (
     <>
       <h1>App gatitos 🐱‍👤</h1>
-      <h2> {data?.fact} </h2>
+
+      <a>
+        {error && <div>Error:{error} </div>}
+        {loading && <div>Loading...</div>}
+        {data?.fact}
+
+      </a>
+      <br /><br />
+      <p>La primera palabra del parrafto es: {primeraPalabra(data?.fact)}  </p>
 
     </>
   )
--- prueba-tecnica/src/useFetch.jsx
+++ prueba-tecnica/src/useFetch.jsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react'
 
 export function useFetch (url) {
   const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => setData(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false))
   }, [])
 
-  return { data }
+  return { data, loading, error }
 }
